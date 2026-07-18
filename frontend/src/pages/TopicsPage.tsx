ceb38b75f9553de29f3e7c0318fa198d84de26ba
Rodrigo Silva <rodrigosilvacto@icloud.com>
2026-07-18 12:09:53 -0300

    Remove unused useState import

--- frontend/src/pages/TopicsPage.tsx
+++ frontend/src/pages/TopicsPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { topicAPI } from '../services/api'
 import { useFilterStore } from '../services/store'
